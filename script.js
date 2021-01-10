0c5c7a3412271ce8246440df7b9fa3845d97e938
raizy21 <chiper_raizy@yahoo.com>
2021-01-10 10:32:06 +0100

    init_nasa-api-pictures

---
+++ script.js
@@ -0,0 +1,14 @@
+/***************
+ *
+ * links
+ * {@link} - https://getloaf.io/
+ * {@link} - https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+ * {@link} - https://api.nasa.gov/
+ * {@link} - https://www.w3schools.com/jsref/jsref_includes.asp
+ * {@link} - https://www.w3schools.com/js/js_json_stringify.asp
+ * {@link} - https://www.w3schools.com/js/js_json_parse.asp
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/delete
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollTo
+ *
+ ****************/
